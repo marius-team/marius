5345f39d38557e5013a9ed1c454565e3ec9eba8c
Jason Mohoney <jasonmohoney@gmail.com>
2021-04-22 11:53:16 -0500

    debug dgl-ke

--- osdi2021/execute.py
+++ osdi2021/execute.py
@@ -61,6 +61,7 @@ def run_marius(config_path, args, show_output=False):
 def run_dglke(args, show_output=False):
     with open("tmp.txt", "w") as tmp_file:
         os.environ["PYTHONUNBUFFERED"] = "1"
+        print(args.split())
         proc = subprocess.Popen(args.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         for line in proc.stdout:
             line = line.decode("utf-8")
