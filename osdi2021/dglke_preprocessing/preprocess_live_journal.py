b2814c6efbccd6572c59146bc20ea4b5c86e4944
Jason Mohoney <jasonmohoney@gmail.com>
2021-04-26 20:55:01 -0500

    update dgl-ke preprocessing

--- osdi2021/dglke_preprocessing/preprocess_live_journal.py
+++ osdi2021/dglke_preprocessing/preprocess_live_journal.py
@@ -33,9 +33,9 @@ def preproccess_live_journal():
             src_nodes = np.asarray(chunk[chunk.columns.values[0]])
             dst_nodes = np.asarray(chunk[chunk.columns.values[1]])
             edges = np.stack([src_nodes, dst_nodes, np.zeros_like(src_nodes)]).T
-
+            np.random.shuffle(edges)
             train, valid, test = np.split(
-                np.random.shuffle(edges),
+                edges,
                 [int(train_fraction*len(chunk)),
                  int((train_fraction + validation_fraction)*len(chunk))])
 
