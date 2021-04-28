904f49c7dc8fead400a67e88f307c3b651fc2b71
Jason Mohoney <jasonmohoney@gmail.com>
2021-04-27 22:22:58 -0500

    update pbg converter for twitter

--- osdi2021/pbg_preprocessing/twitter_to_hdf5.sh
+++ osdi2021/pbg_preprocessing/twitter_to_hdf5.sh
@@ -3,7 +3,7 @@
 BASE_DIR=twitter_16
 NUM_PART=16
 
-CMD="python osdi2021/pbg_preprocessing/parquet_to_hdf5.py --edge_dir_in "freebase16/train" --ent_part_count $NUM_PART --entity_dir "$BASE_DIR/entities" --relation_dir "$BASE_DIR/relations" --edge_dir_out "$BASE_DIR/twitter_train" --output_path "$BASE_DIR/twitter_metadata""
+CMD="python osdi2021/pbg_preprocessing/parquet_to_hdf5.py --edge_dir_in "$BASE_DIR/train" --ent_part_count $NUM_PART --entity_dir "$BASE_DIR/entities" --relation_dir "$BASE_DIR/relations" --edge_dir_out "$BASE_DIR/twitter_train" --output_path "$BASE_DIR/twitter_metadata""
 $CMD
 
 CMD="python osdi2021/pbg_preprocessing/parquet_to_hdf5.py --edge_dir_in "$BASE_DIR/valid" --ent_part_count $NUM_PART --entity_dir None --relation_dir None --edge_dir_out "$BASE_DIR/twitter_valid" --output_path None"
