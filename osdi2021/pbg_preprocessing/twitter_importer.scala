51e7bf3c1683305df6fd611429954d99bd54c5b2
Jason Mohoney <jasonmohoney@gmail.com>
2021-04-27 20:14:57 -0500

    update preprocessing for pbg on twitter

--- osdi2021/pbg_preprocessing/twitter_importer.scala
+++ osdi2021/pbg_preprocessing/twitter_importer.scala
@@ -15,25 +15,12 @@ def preprocess_twitter(path: String) : Dataset[Triplet] = {
     filteredTriplets
 }
 
-def get_twitter_unified() : Dataset[Triplet] = {
-    val twitter = sc.textFile("twitter_16/*edges.txt")
-    val triplets = twitter.map(line => {
-    val parts = line.split(" ")
-    Triplet(parts(0), "0", parts(2))
-    })
 
-    val filteredTriplets = triplets
-        .toDF("subj", "rel", "obj")
-        .as[Triplet]
-
-    filteredTriplets
-}
-
-val unified = get_twitter_unified()
-val train_triplets = preprocess_twitter("twitter_16/train_edges.txt").orderBy(rand())
-val valid_triplets = preprocess_twitter("twitter_16/valid_edges.txt")
-val test_triplets = preprocess_twitter("twitter_16/test_edges.txt")
+val unified = preprocess_twitter("twitter_16/twitter-2010.txt").orderBy(rand())
+val splits = unified.randomSplit(Array(0.9, 0.05, .05), seed = 11L)
+val train_triplets = splits(0)
+val valid_triplets = splits(1)
+val test_triplets = splits(2)
 
 
 create_dataset(unified, train_triplets, valid_triplets, test_triplets, 16, "twitter_16")
-
