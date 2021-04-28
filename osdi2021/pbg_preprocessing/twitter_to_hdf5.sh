45c0ccf4c9a434532c3a688f7369ed68eab66151
Jason Mohoney <jasonmohoney@gmail.com>
2021-04-27 22:19:38 -0500

    update pbg converter for twitter

--- osdi2021/pbg_preprocessing/twitter_to_hdf5.sh
+++ osdi2021/pbg_preprocessing/twitter_to_hdf5.sh
@@ -3,11 +3,11 @@
 BASE_DIR=twitter_16
 NUM_PART=16
 
-CMD="python parquet_to_hdf5.py --edge_dir_in "freebase16/train" --ent_part_count $2 --entity_dir "$BASE_DIR/entities" --relation_dir "$BASE_DIR/relations" --edge_dir_out "$BASE_DIR/twitter_train" --output_path "$BASE_DIR/twitter_metadata""
+CMD="python osdi2021/pbg_preprocessing/parquet_to_hdf5.py --edge_dir_in "freebase16/train" --ent_part_count $2 --entity_dir "$BASE_DIR/entities" --relation_dir "$BASE_DIR/relations" --edge_dir_out "$BASE_DIR/twitter_train" --output_path "$BASE_DIR/twitter_metadata""
 $CMD
 
-CMD="python parquet_to_hdf5.py --edge_dir_in "$BASE_DIR/valid" --ent_part_count $2 --entity_dir None --relation_dir None --edge_dir_out "$BASE_DIR/twitter_valid" --output_path None"
+CMD="python osdi2021/pbg_preprocessing/parquet_to_hdf5.py --edge_dir_in "$BASE_DIR/valid" --ent_part_count $2 --entity_dir None --relation_dir None --edge_dir_out "$BASE_DIR/twitter_valid" --output_path None"
 $CMD
 
-CMD="python parquet_to_hdf5.py --edge_dir_in "$BASE_DIR/test" --ent_part_count $2 --entity_dir None --relation_dir None --edge_dir_out "$BASE_DIR/twitter_test" --output_path None"
+CMD="python osdi2021/pbg_preprocessing/parquet_to_hdf5.py --edge_dir_in "$BASE_DIR/test" --ent_part_count $2 --entity_dir None --relation_dir None --edge_dir_out "$BASE_DIR/twitter_test" --output_path None"
 $CMD
