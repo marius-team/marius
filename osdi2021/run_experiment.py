21bbde207fe08a7dd203e6251cd4ff30b3660b7f
Jason Mohoney <jasonmohoney@gmail.com>
2021-05-15 19:38:47 -0500

    add grid search

--- osdi2021/run_experiment.py
+++ osdi2021/run_experiment.py
@@ -544,12 +544,11 @@ def fb15k_grid_search(overwrite=False, collect_tracing_metrics=False, show_outpu
             command_line_str += "--%s=%s " % (config_keys[i], v)
         command_line_str = command_line_str.strip()
         name = command_line_str.replace(" ", "_").replace("-", "").replace("=", "").replace(".", "")
-        print(command_line_str)
         run_marius(config, exp_dir, name, command_line_str)
         with open(exp_dir + name + "_result.json") as result_file:
             result = json.load(result_file)
             MRR = result["MRR"][-1]
-            print(command_line_str + ": %s" % MRR)
+            print("MRR: %s" % MRR)
 
 
 
