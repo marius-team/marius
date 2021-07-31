42d2cf32112fbfc14a1b1bbc78f7353da1330a38
AnzeXie <axie7@wisc.edu>
2021-07-31 15:01:04 -0500

    Fix query demo

--- examples/inference/wn18/marius_infer.py
+++ examples/inference/wn18/marius_infer.py
@@ -5,22 +5,14 @@ from pathlib import Path
 from marius.tools import postprocess
 
 
-trained_base_dir = Path("./data/")
-output_dir = Path("./output_dir")
-
-# files for node ids in raw data and node ids used by Marius
-# nodes_raw_id_file = "../output_dir/node_mapping.txt"
-# nodes_in_id_file = "../output_dir/node_mapping.bin"
-# nodes_embedding_file = "../training_data/marius/embeddings/embeddings.bin"
-
-
-# files for relation ids in raw data and relation ids used by Marius
-# rels_raw_id_file = "../output_dir/rel_mapping.txt"
-# rels_in_id_file = "../output_dir/rel_mapping.bin"
-# rels_embedding_file = "../training_data/marius/relations/lhs_relations.bin"
+trained_base_dir = Path("../../../data/")
+output_dir = Path("../../../output_dir")
 
 node_id_dict_file = output_dir / Path("node_mapping.txt")
 rel_id_dict_file = output_dir / Path("rel_mapping.txt")
+
+node_emb_file = trained_base_dir / Path("marius/embeddings/embeddings.bin")
+rel_emb_file = trained_base_dir / Path("marius/relations/src_relations.bin")
 
 # definition files for nodes ids in raw data
 def_file = output_dir / Path("wordnet-mlj12-definitions.txt")
@@ -29,27 +21,17 @@ def_file = output_dir / Path("wordnet-mlj12-definitions.txt")
 # return tensor form embeddings and embeddings dict
 def tensor_from_file(choice, is_src_rel=True):
     if choice == "node":
-        # raw_id_file = nodes_raw_id_file
-        # embedding_file = nodes_embedding_file
-        emb_dict = postprocess.get_emb_dicts()[0]
-        embs = list(emb_dict.values())
+        mapping = dict(np.loadtxt(node_id_dict_file, dtype=str, delimiter='\t'))
+        embs = postprocess.get_embs(node_id_dict_file, node_emb_file)
     else:
-        # raw_id_file = rels_raw_id_file
-        # embedding_file = rels_embedding_file
-        if is_src_rel == True:
-            emb_dict = postprocess.get_emb_dicts()[1]
-        else:
-            emb_dict = postprocess.get_emb_dicts()[2]
-        
-        embs = list(emb_dict.values())
+        mapping = dict(np.loadtxt(rel_id_dict_file, dtype=str, delimiter='\t'))
+        embs = postprocess.get_embs(rel_id_dict_file, rel_emb_file)
 
-    # raw_ids = []
-    # with open(raw_id_file) as f:
-    #     for line in f.readlines():
-    #         raw_ids.append(line.strip())
+    emb_dict = []
+    for k in mapping.keys():
+        emb_dict.append([k, embs[int(mapping.get(k))]])
+    emb_dict = dict(emb_dict)
 
-    # num = len(raw_ids)
-    # embeddings = np.fromfile(embedding_file, np.float32).reshape((num, -1))
     return torch.tensor(embs), emb_dict
 
 
@@ -58,7 +40,7 @@ def lookup_embedding(choice, id, emb_dict):
     if choice == "node":
         id = def_dict.get(id)
 
-    return emb_dict.get(id)
+    return emb_dict.get(str(id))
 
 
 # read in definition from raw data
