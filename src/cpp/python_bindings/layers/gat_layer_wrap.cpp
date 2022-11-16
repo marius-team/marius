c2c4240a8cb3e3f09eb4a58a2d59ab9080fe2e1c
Roger Waleffe <rogerwaleffe@Rogers-MacBook-Pro.local>
2022-11-16 12:27:10 -0600

    update gat python bindings to match gat layer update from previous commit

--- src/cpp/python_bindings/layers/gat_layer_wrap.cpp
+++ src/cpp/python_bindings/layers/gat_layer_wrap.cpp
@@ -11,6 +11,8 @@ void init_gat_layer(py::module &m) {
      torch::python::bind_module<GATLayer>(m, "GATLayer")
           .def_readwrite("layer_config", &GATLayer::layer_config_)
           .def_readwrite("options", &GATLayer::options_)
+          .def_readwrite("use_incoming", &GATLayer::use_incoming_)
+          .def_readwrite("use_outgoing", &GATLayer::use_outgoing_)
           .def_readwrite("head_dim", &GATLayer::head_dim_)
           .def_readwrite("input_dropout", &GATLayer::input_dropout_)
           .def_readwrite("attention_dropout", &GATLayer::attention_dropout_)
@@ -19,8 +21,10 @@ void init_gat_layer(py::module &m) {
           .def_readwrite("a_r", &GATLayer::a_r_)
           .def_readwrite("bias", &GATLayer::bias_)
           .def_readwrite("device", &GATLayer::device_)
-          .def(py::init<shared_ptr<GNNLayerConfig>, torch::DeviceType>(),
+          .def(py::init<shared_ptr<GNNLayerConfig>, bool, bool, torch::DeviceType>(),
                py::arg("layer_config"),
+               py::arg("use_incoming"),
+               py::arg("use_outgoing"),
                py::arg("device"))
           .def("reset", &GATLayer::reset)
           .def("forward", &GATLayer::forward,
