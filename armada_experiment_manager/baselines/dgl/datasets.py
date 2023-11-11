6d6cf898f72b527503e954db12ce7323dc48a7e0
Roger Waleffe <rogerwaleffe@Rogers-MBP.attlocal.net>
2023-11-10 19:39:14 -0600

    initial dgl baseline

---
+++ armada_experiment_manager/baselines/dgl/datasets.py
@@ -0,0 +1,184 @@
+import dgl
+import torch
+import numpy as np
+
+from omegaconf import OmegaConf
+
+
+class MariusLPDataset(object):
+
+    def __init__(self, base_directory, add_reverse_edges=True, only_train=False):
+        self.base_directory = base_directory
+        self.add_reverse_edges = add_reverse_edges
+        self.only_train = only_train
+        self.graph = None
+
+    def process(self):
+        base_directory = self.base_directory.rstrip("/")
+
+        dataset_stats = OmegaConf.load(base_directory + "/dataset.yaml")
+
+        num_columns = 3
+        if dataset_stats.num_relations == 1:
+            num_columns = 2
+
+        if self.only_train:
+            edges = [torch.from_numpy(np.fromfile(base_directory + "/edges/train_edges.bin",
+                                                  dtype=np.int32)).view(-1, num_columns)]
+
+            edges = torch.cat(edges)
+        else:
+            train_edges = torch.from_numpy(np.fromfile(base_directory + "/edges/train_edges.bin",
+                                                       dtype=np.int32)).view(-1, num_columns)
+            valid_edges = torch.from_numpy(np.fromfile(base_directory + "/edges/validation_edges.bin",
+                                                       dtype=np.int32)).view(-1, num_columns)
+            test_edges = torch.from_numpy(np.fromfile(base_directory + "/edges/test_edges.bin",
+                                                      dtype=np.int32)).view(-1, num_columns)
+
+            edges = [train_edges, valid_edges, test_edges]
+            edges = torch.cat(edges)
+
+            if self.add_reverse_edges:
+                train_mask = torch.zeros([edges.size(0) * 2], dtype=torch.bool)
+                valid_mask = torch.zeros([edges.size(0) * 2], dtype=torch.bool)
+                test_mask = torch.zeros([edges.size(0) * 2], dtype=torch.bool)
+
+                train_mask[:dataset_stats.num_train] = True
+                valid_mask[dataset_stats.num_train:dataset_stats.num_train + dataset_stats.num_valid] = True
+                test_mask[dataset_stats.num_train + dataset_stats.num_valid:dataset_stats.num_train + dataset_stats.num_valid + dataset_stats.num_test] = True
+            else:
+                train_mask = torch.zeros([edges.size(0)], dtype=torch.bool)
+                valid_mask = torch.zeros([edges.size(0)], dtype=torch.bool)
+                test_mask = torch.zeros([edges.size(0)], dtype=torch.bool)
+
+                train_mask[:dataset_stats.num_train] = True
+                valid_mask[dataset_stats.num_train:dataset_stats.num_train + dataset_stats.num_valid] = True
+                test_mask[dataset_stats.num_train + dataset_stats.num_valid:] = True
+
+        edges_src = edges[:, 0]
+        edges_dst = edges[:, -1]
+
+        if self.add_reverse_edges:
+            new_edges_src = torch.cat([edges_src, edges_dst])
+            new_edges_dst = torch.cat([edges_dst, edges_src])
+
+            edges_src = new_edges_src
+            edges_dst = new_edges_dst
+
+        self.graph = dgl.graph((edges_src, edges_dst), num_nodes=dataset_stats.num_nodes)
+
+        if num_columns == 3:
+            edges_type = edges[:, 1]
+            if self.add_reverse_edges:
+                edges_type = torch.cat([edges_type, edges_type])
+                self.graph.edata['etype'] = edges_type
+            else:
+                self.graph.edata['etype'] = edges_type
+
+        if not self.only_train:
+            self.graph.edata['train_mask'] = train_mask
+            self.graph.edata['val_mask'] = valid_mask
+            self.graph.edata['test_mask'] = test_mask
+
+    def __getitem__(self, i):
+        return self.graph
+
+    def __len__(self):
+        return 1
+
+
+class MariusNCDataset(object):
+
+    def __init__(self, base_directory, add_reverse_edges=True, only_sample=False):
+        self.base_directory = base_directory
+        self.add_reverse_edges = add_reverse_edges
+        self.only_sample = only_sample
+        self.graph = None
+
+    def process(self):
+        base_directory = self.base_directory.rstrip("/")
+
+        dataset_stats = OmegaConf.load(base_directory + "/dataset.yaml")
+
+        num_columns = 3
+        if dataset_stats.num_relations == 1:
+            num_columns = 2
+
+        train_edges = torch.from_numpy(np.fromfile(base_directory + "/edges/train_edges.bin",
+                                                   dtype=np.int32)).view(-1, num_columns)
+
+        edges_src = train_edges[:, 0]
+        edges_dst = train_edges[:, -1]
+
+        if self.add_reverse_edges:
+            new_edges_src = torch.cat([edges_src, edges_dst])
+            new_edges_dst = torch.cat([edges_dst, edges_src])
+
+            edges_src = new_edges_src
+            edges_dst = new_edges_dst
+
+        train_nodes = torch.from_numpy(np.fromfile(base_directory + "/nodes/train_nodes.bin", dtype=np.int32))
+        valid_nodes = torch.from_numpy(np.fromfile(base_directory + "/nodes/validation_nodes.bin", dtype=np.int32))
+        test_nodes = torch.from_numpy(np.fromfile(base_directory + "/nodes/test_nodes.bin", dtype=np.int32))
+
+        num_nodes = dataset_stats.num_nodes
+        num_relations = dataset_stats.num_relations
+
+        # DGL was throwing cuda errors with things as int32
+        edges_src = edges_src.to(torch.int64)
+        edges_dst = edges_dst.to(torch.int64)
+        self.graph = dgl.graph((edges_src, edges_dst), num_nodes=dataset_stats.num_nodes)
+
+        if num_columns == 3:
+            edges_type = train_edges[:, 1]
+            if self.add_reverse_edges:
+                edges_type = torch.cat([edges_type, edges_type])
+                self.graph.edata['etype'] = edges_type
+            else:
+                self.graph.edata['etype'] = edges_type
+
+        train_mask = torch.zeros(num_nodes, dtype=torch.bool)
+        val_mask = torch.zeros(num_nodes, dtype=torch.bool)
+        test_mask = torch.zeros(num_nodes, dtype=torch.bool)
+
+        train_mask[train_nodes.to(torch.int64)] = True
+        val_mask[valid_nodes.to(torch.int64)] = True
+        test_mask[test_nodes.to(torch.int64)] = True
+
+        self.graph.ndata['train_mask'] = train_mask
+        self.graph.ndata['val_mask'] = val_mask
+        self.graph.ndata['test_mask'] = test_mask
+
+        if not self.only_sample:
+            features = torch.from_numpy(np.fromfile(base_directory + "/nodes/features.bin",
+                                                    dtype=np.float32)).view(-1, dataset_stats.node_feature_dim)
+            labels = torch.from_numpy(np.fromfile(base_directory + "/nodes/labels.bin", dtype=np.int32))
+            labels = labels.to(torch.int64)
+
+            self.graph.ndata['feat'] = features
+            self.graph.ndata['label'] = labels
+
+    def __getitem__(self, i):
+        return self.graph
+
+    def __len__(self):
+        return 1
+
+
+def get_marius_dataset_lp(base_directory, add_reverse_edges, only_train=False):
+    dataset = MariusLPDataset(base_directory, add_reverse_edges, only_train=False)
+    dataset.process()
+    return dataset[0]
+
+
+def get_marius_dataset_nc(base_directory, add_reverse_edges, only_sample=False):
+    dataset = MariusNCDataset(base_directory, add_reverse_edges, only_sample)
+    dataset.process()
+    return dataset[0]
+
+
+def select_dataset(base_directory, learning_task, add_reverse_edges):
+    if learning_task.upper() == "LINK_PREDICTION":
+        return get_marius_dataset_lp(base_directory, add_reverse_edges)
+    else:
+        return get_marius_dataset_nc(base_directory, add_reverse_edges)
---
+++ armada_experiment_manager/baselines/dgl/run_dgl.py
@@ -0,0 +1,88 @@
+import argparse
+
+from node_classification import run_nc
+from link_prediction import run_lp
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+
+    # [dataset_options]
+    p.add_argument("--base_directory")
+    p.add_argument("--learning_task", default="link_prediction")
+
+    # [emb_options]
+    p.add_argument("--emb_dim", type=int, default=50)
+    p.add_argument("--emb_storage_device", type=str, default="CPU")
+    p.add_argument("--emb_storage_backend", type=str, default="dgl_sparse")
+
+    # [encoder/decoder_options]
+    p.add_argument("--encode", type=str2bool, default=True) # not necessarily optimized for no encode
+    p.add_argument("--model", default="graph_sage")
+    p.add_argument("--outgoing_nbrs", type=str2bool, default=True)
+    p.add_argument("--num_layers", type=int, default=1)
+    p.add_argument("--h_dim", type=int, default=50)
+    p.add_argument("--out_dim", type=int, default=50)
+
+    # [model_options]
+    p.add_argument("--graph_sage_aggregator", default="mean")
+    p.add_argument("--graph_sage_dropout", type=float, default=0.0)
+    p.add_argument("--gat_num_heads", type=int, default=10)
+    p.add_argument("--gat_feat_drop", type=float, default=0.0)
+    p.add_argument("--gat_attn_drop", type=float, default=0.0)
+    p.add_argument("--gat_negative_slope", type=float, default=0.2)
+
+    # [train_options]
+    p.add_argument("--train_batch_size", type=int, default=1000)
+    p.add_argument("--single_format", type=str2bool, default=False)
+    p.add_argument("--num_gpus", type=int, default=1)
+    p.add_argument("--sample_device", type=str, default="CPU")
+    p.add_argument("--num_epochs", type=int, default=10)
+    p.add_argument("--learning_rate", type=float, default=0.1)
+    p.add_argument("--optimizer", type=str, default="Adagrad")
+    p.add_argument("--num_train_nbrs", nargs="+", type=int, default=[-1])
+    p.add_argument("--num_train_chunks", type=int, default=10)
+    p.add_argument("--num_train_uniform_negs", type=int, default=1000)
+    p.add_argument("--num_train_deg_negs", type=int, default=0)
+
+    # [eval_options]
+    p.add_argument("--epochs_per_eval", type=int, default=1)
+    p.add_argument("--eval_batch_size", type=int, default=1000)
+    p.add_argument("--filtered_mrr", type=str2bool, default=False)
+    p.add_argument("--num_eval_nbrs", nargs="+", type=int, default=[-1])
+    p.add_argument("--num_eval_chunks", type=int, default=10)
+    p.add_argument("--num_eval_uniform_negs", type=int, default=1000)
+    p.add_argument("--num_eval_deg_negs", type=int, default=0)
+
+    # [performance_options]
+    p.add_argument("--prefetch_neighbor_sampler", type=str2bool, default=True)
+    p.add_argument("--use_uva", type=str2bool, default=False)
+    p.add_argument("--use_alternate_streams", type=str2bool, default=True)
+    p.add_argument("--use_prefetch_thread", type=str2bool, default=True)
+    p.add_argument("--pin_prefetcher", type=str2bool, default=True)
+    p.add_argument("--num_workers", type=int, default=0)
+    p.add_argument("--prefetch_factor", type=int, default=1)
+    p.add_argument("--persistent_workers", type=str2bool, default=False)
+
+    p.add_argument('--print_timing', action='store_true', default=False)
+    p.add_argument('--only_sample', action='store_true', default=False)
+    p.add_argument('--no_compute', action='store_true', default=False)
+
+    config_args = p.parse_args()
+
+    if config_args.learning_task.upper() == "LINK_PREDICTION":
+        run_lp(config_args)
+    else:
+        run_nc(config_args)
+
