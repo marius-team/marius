b6b5d521b0bc0bdd54c0fb2480c5dacc51a49a0b
dsarda <dsarda@cosmos0003.chtc.wisc.edu>
2024-09-11 17:12:18 -0500

    Latest version of code

--- numa_benchmarking/main.cpp
+++ numa_benchmarking/main.cpp
@@ -3,12 +3,14 @@
 #include <cstring>
 #include <numa.h>
 #include <simsimd/simsimd.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 
 // Define the necessary constants
 const int vector_size = 128;
 const size_t ONE_GB = 1024 * 1024 * 1024;  
-const float NUM_CHUNKS = 10;
+const float NUM_CHUNKS = 0.1;
 const size_t BUFFER_SIZE = NUM_CHUNKS * ONE_GB; // Buffer size of 10 GB
 
 void benchmark_scan_list(float* query_vec, float* search_vectors, size_t num_vectors, std::string benchmark_name) {
@@ -17,9 +19,9 @@ void benchmark_scan_list(float* query_vec, float* search_vectors, size_t num_vec
 
     // Run the actual benchmark
     auto start = std::chrono::high_resolution_clock::now();
-    for (size_t j = 0; j < num_vectors; j++) {
-        float* curr_search_vec = search_vectors + j * vector_size;
-        simsimd_l2sq_f32(query_vec, curr_search_vec, vector_size, &dist_result);
+    for (size_t i = 0; i < num_vectors; i++) {
+        float* curr_search_vec = search_vectors + i * vector_size;
+        simsimd_dot_f32(query_vec, curr_search_vec, vector_size, &dist_result);
         total_distance += dist_result;
     }
     auto end = std::chrono::high_resolution_clock::now();
@@ -44,8 +46,11 @@ int main() {
         std::cerr << "Failed to force worker to run on node 0" << std::endl;
     }
 
+    int random_fd = open("/dev/random", O_RDONLY);
+
     // Initialize the query vector
     float query_vector[vector_size];
+    read(random_fd, query_vector, vector_size * sizeof(float));
 
     // Create the target vector on both nodes
     float* node_zero_vectors = reinterpret_cast<float*>(numa_alloc_onnode(BUFFER_SIZE, 0));
@@ -54,10 +59,15 @@ int main() {
         std::cerr << "Failed to allocate memory on numa nodes" << std::endl;
         numa_free(node_zero_vectors, BUFFER_SIZE);
         numa_free(node_one_vectors, BUFFER_SIZE);
+        close(random_fd);
         return 1;
     }
     size_t single_vector_size = vector_size * sizeof(float);
     size_t num_vectors = BUFFER_SIZE/single_vector_size;
+
+    // Populate both the buffers with data
+    read(random_fd, node_one_vectors, BUFFER_SIZE);
+    read(random_fd, node_zero_vectors, BUFFER_SIZE);
 
     // Run the cross node benchmark
     std::cout << "Running benchmarking for buffer size of " << NUM_CHUNKS << " GB" << std::endl;
@@ -67,6 +77,7 @@ int main() {
     // Run the same node benchmark
     benchmark_scan_list(query_vector, node_zero_vectors, num_vectors, "Node0 Vectors");
     numa_free(node_zero_vectors, BUFFER_SIZE);
+    close(random_fd);
 
     return 0;
 }
